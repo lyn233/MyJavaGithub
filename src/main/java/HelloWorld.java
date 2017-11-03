7b13c9e5255e51ccbc4ae04bbd7f170768971172
daiguanlin <daiguanlin@126.com>
2017-11-03 20:59:19 +0800

    接收参数

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,13 +3,25 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @EnableAutoConfiguration
 public class HelloWorld {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    String index(String name) {
+        String total;
+        total = name + " says hello world";
+        return total;
+    }
+
+    @RequestMapping("/order")
+    String order(HttpServletRequest request){
+        String order_id = request.getParameter("order_id");
+        System.out.println(order_id);
+        String total = order_id + "order_id";
+        return total;
     }
 
     public static void main(String[] args) throws Exception {
